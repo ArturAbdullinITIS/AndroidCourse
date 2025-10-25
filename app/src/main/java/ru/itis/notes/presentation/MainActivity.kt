a8617f57438247478383a4ca2bd42931584c0a99
ArturAbdullinITIS <arturaubdullin@gmail.com>
2025-10-25 20:12:04 +0300

    Added MainActivity
    app entry point

---
+++ app/src/main/java/ru/itis/notes/presentation/MainActivity.kt
@@ -0,0 +1,29 @@
+package ru.itis.notes.presentation
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.itis.notes.presentation.navigation.NavGraph
+import ru.itis.notes.presentation.screens.first.RegistrationScreen
+import ru.itis.notes.presentation.ui.theme.NotesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NotesTheme {
+                NavGraph()
+            }
+        }
+    }
+}
